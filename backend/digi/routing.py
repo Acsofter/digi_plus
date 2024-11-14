cf04c3b8ed3341b43626502829a7e8271c6c54f7
Acsofter <32777537+Acsofter@users.noreply.github.com>
2024-11-14 15:46:02 -0400

    Update routing.py

--- backend/digi/routing.py
+++ backend/digi/routing.py
@@ -2,6 +2,6 @@ from django.urls import re_path
 from .consumers.company import CompanyConsumer
 
 websocket_urlpatterns = [
-    re_path(r'ws/company/(?P<room_name>\w+)/$', CompanyConsumer.as_asgi()),
+    re_path(r'ws/company/$', CompanyConsumer.as_asgi()),
     # re_path(r'ws/pm/(?P<user_name>\w+)/$', PMConsumer.as_asgi()),
 ]
