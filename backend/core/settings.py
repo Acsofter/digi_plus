4290c294c681724ae9b5b4ee6826389c9b3a234d
Acsofter <32777537+Acsofter@users.noreply.github.com>
2024-11-14 12:32:26 -0400

    Update settings.py

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'digi.apps.DigiConfig',
     'daphne',
+    'channels',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
